7b63f6025cca6ddbf459a08dc7db8bd72af45d8a
SergCherkasov <serg.cherkasov@gmail.com>
2023-01-31 16:36:42 +0200

    GetPriority method

--- HW_3/CalculatorMechanics/CalculatorMechanics.cs
+++ HW_3/CalculatorMechanics/CalculatorMechanics.cs
@@ -24,5 +24,19 @@ namespace HW_3.CalculatorMechanics
             }
             return false;
         }
+        private byte GetPriority(char c)
+        {
+            switch (c)
+            {
+                case '(': return 0;
+                case ')': return 1;
+                case '+': return 2;
+                case '-': return 3;
+                case '*': return 4;
+                case '/': return 5;
+
+                default: return 6;
+            }
+        }
     }
 }
