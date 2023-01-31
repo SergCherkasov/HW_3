94ce8f5813b83a87a9b79793544f605827ac1ff8
SergCherkasov <serg.cherkasov@gmail.com>
2023-01-31 20:26:16 +0200

    Test "SubstractInteger"

--- TestsForCalculator/CalculatorTests.cs
+++ TestsForCalculator/CalculatorTests.cs
@@ -44,6 +44,16 @@ namespace TestsForCalculator
             var actualResult = cm.Counting(transformedExpression);
             Assert.AreEqual(actualResult, expectedResult);
         }
-        
+        [TestMethod]
+        [DataRow(2, 2, 1)]
+        [DataRow(100, 10, 10)]
+        [DataRow(4, -2, -2)]
+        public void SubstractInteger(int firstNumber, int secondNumber, int expectedResult)
+        {
+            string inputExpression = $"{firstNumber}/{secondNumber}";
+            string transformedExpression = cm.TransformExpression(inputExpression);
+            var actualResult = cm.Counting(transformedExpression);
+            Assert.AreEqual(actualResult, expectedResult);
+        }
     }
 }
