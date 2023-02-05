83be3670d852ab8c8cc827ba9e672b29271acad7
SergCherkasov <serg.cherkasov@gmail.com>
2023-02-05 12:51:43 +0200

    Realization TestConsole

---
+++ TestsForCalculator/ConsoleTests.cs
@@ -0,0 +1,28 @@
+﻿using HW_3.UserInterface;
+namespace TestsForCalculator
+{
+    [TestClass]
+
+    public class ConsoleTests
+    {
+        [TestMethod]
+
+        public void TestConsoleByAdding()
+        {
+            var textReader = new StringReader("2+7");
+            Console.SetIn(textReader);
+
+            var stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+            
+
+            UserInterface ui = new UserInterface();
+
+            ui.Calculate();
+
+            var result=stringWriter.ToString();
+            Assert.AreEqual(result, "\tWELCOME TO CALCULATOR APP!\r\n\r\nEnter expression:\r\n\r\nResult of Your expression:\r\n9\r\n");
+            
+        }
+    }
+}
